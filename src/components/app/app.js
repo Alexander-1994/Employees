db33b8dafbc7d972e848a86a605b5428c42c470e
Alexander <kisliy.doma@gmail.com>
2022-08-11 21:18:02 +0800

    state

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,12 +9,12 @@ import './app.css';
 
 function App() {
     const data = [                                                      /* Импровизация данных с сервера -> передадим их в компонент EmployeesList в data-пропсе ниже */
-        {name: 'John S.', salary: 800, increase: true, id: 111111},
-        {name: 'Alex K.', salary: 7000, increase: false, id: 222222},
-        {name: 'Ann C.', salary: 2000, increase: true, id: 333333},
-        {name: 'Oleg A.', salary: 3000, increase: false, id: 444444},
-        {name: 'Dvitriy S.', salary: 1700, increase: false, id: 555555},
-        {name: 'Kianu R.', salary: 20000, increase: false, id: 666666}
+        {name: 'John S.', salary: 800, id: 111111},
+        {name: 'Alex K.', salary: 7000, id: 222222},
+        {name: 'Ann C.', salary: 2000, id: 333333},
+        {name: 'Oleg A.', salary: 3000, id: 444444},
+        {name: 'Dvitriy S.', salary: 1700, id: 555555},
+        {name: 'Kianu R.', salary: 20000, id: 666666}
     ];
 
     return (
